0d1721ef8336bdc967db51e11f2cd5a84113e188
movleax <6619780+movleax@users.noreply.github.com>
2019-01-11 11:48:53 -0800

    Added snakeNodeSwaure to represent the width and height.This will be used in creating a new body node.Modified the Vector2D class a bit.Vector2D has AddVector, GetDegrees, and Rotation.Created some interfaces,but not sure if they will be used since we are using JS.Implemented BodyNode.WIP implementing Snake.

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ ctx.fillRect(20, 20, 150, 100);
 
 var gameLoopInterval = setInterval(GameCycle, simSpeed);
 
+const snakeNodeSquare = new Vector2D(10, 10);
+
 /* 
 Snake:
 	Has a head node
@@ -28,6 +30,8 @@ class Vector2D
     {
         this.x = x == undefined ? 0 : x;
         this.y = y == undefined ? 0 : y;
+
+        this.rads = Math.atan2(y, x);
     }
 
     GetVector2D()
@@ -39,6 +43,43 @@ class Vector2D
     {
         this.x = x;
         this.y = y;
+
+        this.rads = Math.atan2(y, x);
+    }
+
+    AddVector(vector)
+    {
+        let newX = this.x + vector.x;
+        let newY = this.y + vector.y;
+
+        return new Vector2D(newX, newY);
+    }
+
+    GetDegrees()
+    {
+        return this.degrees;
+    }
+
+    RotateAroundOrigin(degrees)
+    {
+        let newRads = (degrees  * Math.PI / 180);
+
+        // perform the rotation
+        let rotatedX = Math.cos(newRads) * (this.x) - Math.sin(newRads) * (this.y);
+        let rotatedY = Math.sin(newRads) * (this.x) + Math.cos(newRads) * (this.y);
+    
+        return new Vector2D(rotatedX, rotatedY);
+    }
+
+    RotateAroundPoint(point, degrees)
+    {
+        let newRads = (degrees  * Math.PI / 180);
+
+        // perform the rotation
+        let rotatedX = Math.cos(newRads) * (this.x - point.x) - Math.sin(newRads) * (this.y - point.y) + point.x;
+        let rotatedY = Math.sin(newRads) * (this.x - point.x) + Math.cos(newRads) * (this.y - point.y) + point.y;
+        
+        return new Vector2D(rotatedX, rotatedY);
     }
 }
 
@@ -108,22 +149,64 @@ class iDrawable
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, 100, 100);
         ctx.fillStyle = "black";
-        ctx.fillText("Must Implement Must override iDrawable's Draw() method", 0, 0, 100);
+        ctx.fillText("Must override iDrawable's Draw() method", 0, 0, 100);
+    }
+}
+
+class iMoveable
+{
+    constructor()
+    {
+        this.velocity;
     }
 }
 
 
-
-
-
-
-
-
-class BodyNodes
+class BodyNode extends iDrawable
 {
-    constructor()
+    constructor(x, y, w, h, direction)
     {
+        this.cBox = new CollisionBox(x, y, w, h);
+        this.position = new Vector2D(x, y);
+        this.direction = direction;
+        this.node = null;
+    }
 
+    AddBodyNode()
+    {
+        if(this.node == null)
+        {
+            let bodyNode = new BodyNode(this.x, this.y, this.w, this.h, this.direction);
+
+            // calculate the position vector behind *this body node
+            let oppositeDirection = this.direction.RotateAroundOrigin(this.direction.GetDegrees()+180);
+            let offsetVector = new Vector2D(oppositeDirection.x*snakeNodeSquare.x, oppositeDirection.y*snakeNodeSquare.y);
+            let behindNodePosition = this.position.AddVector(offsetVector);
+
+            bodyNode.position = behindNodePosition;
+
+            this.node = bodyNode;
+        }
+        else
+        {
+            this.node.AddBodyNode();
+        }
+    }
+
+    UpdatePosition(newPosition)
+    {
+        if(this.node != null)
+        {
+            this.node.UpdatePosition(this.position);
+        }
+
+        this.position = newPosition;
+    }
+
+    Draw(ctx)
+    {
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.position.x, this.position.y, snakeNodeSquare.x, snakeNodeSquare.y);
     }
 }
 
@@ -131,11 +214,27 @@ class BodyNodes
 
 class Snake
 {
-    constructor()
+    constructor(x, y, w, h)
+    {
+
+        let head = new BodyNode(x, y, w, h, new Vector2D(1, 0));
+        head.AddBodyNode();
+    }
+
+    Draw(ctx)
+    {
+        let traverseNode = head;
+        while(traverseNode != null)
+        {
+            traverseNode.Draw(ctx);
+            traverseNode = traverseNode.node;
+        }
+    }
+
+    Move()
     {
 
     }
-
  
 }
 
